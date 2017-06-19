4a81d2ef3ee93c1399bee6550656a1fd6c758bce
Rainfore <rainforest92@126.com>
2017-06-19 20:18:12 +0800

    :wheelchair: Add --config-path option

--- src/getVusionConfig.js
+++ src/getVusionConfig.js
@@ -1,20 +1,8 @@
 const fs = require('fs');
 const path = require('path');
 
-const vusionConfigPath = path.join(process.cwd(), 'vusion.config.js');
-const boardConfigPath = path.join(process.cwd(), 'board.config.js');
-const serverConfigPath = path.join(process.cwd(), 'server.config.js');
-let vusion, board, server;
-
-if (fs.existsSync(vusionConfigPath))
-    vusion = require(vusionConfigPath);
-if (fs.existsSync(boardConfigPath))
-    board = require(boardConfigPath);
-if (fs.existsSync(serverConfigPath))
-    server = require(serverConfigPath);
-
-module.exports = {
-    vusion,
-    board,
-    server,
+module.exports = function (relativePath = 'vusion.config.js') {
+    const configPath = path.join(process.cwd(), relativePath);
+    if (fs.existsSync(configPath))
+        return require(configPath);
 };
