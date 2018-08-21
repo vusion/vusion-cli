c5ddc47bd803097fffde4c9c6b764a41424f5c14
kewen <yqdawujiang@163.com>
2018-08-21 16:51:46 +0800

    :bug: fix-test
    fix-test

--- test/dev.test.js
+++ test/dev.test.js
@@ -52,7 +52,7 @@ describe('command: dev', () => {
         });
 
         it('should run with local config', (done) => {
-            const childProcess = execa(devCli, ['-e', 'index.js', '-C', '-d', 'true', '-p', '4430', '-H'], {
+            const childProcess = execa(devCli, ['-e', 'index.js', '-d', 'true', '-p', '4430', '-H'], {
                 stdio: ['ipc'],
             });
 
@@ -61,7 +61,6 @@ describe('command: dev', () => {
                 expect(entryPath).to.equal('index.js');
                 expect(port).to.equal(4430);
                 /* eslint-disable no-unused-expressions */
-                expect(clean).to.be.false;
                 expect(docs).to.be.true;
                 expect(hot).to.be.false;
                 /* eslint-enable no-unused-expressions */
