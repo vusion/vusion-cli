d6da7bbb28d2df71a6bf00c85a4f1d1659087f4d
Rainfore <rainforest92@126.com>
2019-02-26 19:11:04 +0800

    :ok_hand: Review postcss-extend

--- test/features/postcss-extend/src/index.js
+++ test/features/postcss-extend/src/index.js
@@ -1,10 +1,10 @@
-import './case-simple.css';
+// import './case-simple.css';
 import './case-simple-import.css';
-import './case-multiple.css';
-import './case-multiple-import.css';
-import './case-mixed.css';
-import './case-complex.css';
-import './case-complex-import.css';
+// import './case-multiple.css';
+// import './case-multiple-import.css';
+// import './case-mixed.css';
+// import './case-complex.css';
+// import './case-complex-import.css';
 
 // import Vue from 'vue';
 // import Button from './u-extended-button.vue';
