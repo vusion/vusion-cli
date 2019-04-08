f8f1d5236fc933d9b954b1acf8a28fa684faa39b
Rainfore <rainforest92@126.com>
2019-04-08 16:48:31 +0800

    :boom: Use current as default

--- config/defaults.js
+++ config/defaults.js
@@ -24,7 +24,7 @@ module.exports = {
     minifyJS: false,                       // Minify JS only in `build` mode. Set `true` or 'babel-minify' to use BabelBabelMinifyWebpackPlugin, set 'uglify-js' to use UglifyjsWebpackPlugin as same as `uglifyJS: true`
     forceShaking: false,                   // Force to enable tree shaking under this path without care of side effects. It's different from default tree shaking of webpack.
     experimental: false,                   // Enable some experimental loaders or plugins, like ModuleConcatenationPlugin
-    resolvePriority: 'cwd',                // Priority to resolve modules or loaders, "cwd"(default) or "cli"
+    resolvePriority: 'current',                // Priority to resolve modules or loaders, "cwd"(default) or "cli"
     browsers: ['> 1%', 'last 2 versions', 'ie >= 9'],    // Browers Compatibility referred in autoprefixer. See browserslist for more details
     babelIncludes: [],                     // Reinclude some files excluded in node_modules
     webpack: {},                           // Extend webpack configuration
